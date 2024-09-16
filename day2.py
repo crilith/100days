360752cc016848b3cb6409b004af32458b2c2dab
crilith <unrefinedthought@gmail.com>
2024-09-16 00:00:50 -0500

    day2

---
+++ day2.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+total_bill=float(input("What was the total bill? $"))
+tip_percent=float(input("What percentage tip would you like to give? "))
+people=int(input("How many people to split the bill? "))
+formatted_answer="{:.{}f}".format(total_bill * (1+tip_percent/100) / people, 2)
+print(f"Each person should pay: ${formatted_answer}")

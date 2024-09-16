8af0396cfa756705dc3987c66b70588fa7a0105a
crilith <unrefinedthought@gmail.com>
2024-09-15 23:11:06 -0500

    day1

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in? ")
+pets_name = input("What's your pets name? ")
+print(f"Your band name could be {city_name} {pets_name}.")

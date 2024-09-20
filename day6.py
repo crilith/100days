b48d5b318d546afaa741c2e98085542d9a7c34b0
crilith <unrefinedthought@gmail.com>
2024-09-20 01:49:30 -0500

    day6 - solving a maze

---
+++ day6.py
@@ -0,0 +1,17 @@
+# This code solves the maze at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+
+# Still need to go back and figure out how to solve the problem if it's going in a square.
